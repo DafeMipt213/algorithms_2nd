227b6fdd9df83ac5cb76740b541b63204948a4df
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-11-18 10:32:43 +0000

    task 3 done

--- sandbox/template/src/main.cpp
+++ sandbox/template/src/main.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
+#include "utils.hpp"
 
-int main() { return 0; }
+
+int main() {
+    
+    std::vector<int> pasha{1, 10, 7, 6, 3, 2, 1, 11};
+    RMQ pashka(pasha);
+    std::cout << pashka.findMin(3, 4) << '\n';
+    return 0; 
+}
