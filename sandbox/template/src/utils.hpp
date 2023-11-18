32a09e9d06dd749c6bf1fbadb30f0bf967ea98d0
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-11-18 10:32:43 +0000

    task 3 done

--- sandbox/template/src/utils.hpp
+++ sandbox/template/src/utils.hpp
@@ -1,1 +1,14 @@
 #pragma once
+
+#include <vector>
+#include <cmath>
+
+class RMQ{
+    std::vector<std::vector<int>> minimum_table;
+
+    public:
+    RMQ(const std::vector<int>& input_vector);
+
+    int findMin(const unsigned int left, const unsigned int right);
+
+};
