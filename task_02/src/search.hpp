221461ee9f9a7bf5b7402bf518c5a26ca2027f41
VariPor <116594957+VariPor@users.noreply.github.com>
2023-11-18 14:05:47 +0000

    task2

---
+++ task_02/src/search.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+
+// для неориентированного графа без кратных рёбер, задаваемого списком смежности
+
+using Graph = std::vector<std::vector<char>>;
+struct edge{
+    char start;
+    char end;
+    edge(char start, char end) : start{start}, end{end} {}
+};
+
+std::vector<edge> bridge_search(const Graph& graph);
+std::vector<char> dots_search(const Graph& graph);
