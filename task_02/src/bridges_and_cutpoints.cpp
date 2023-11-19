b427324f4062d2cb1f4f56d59229f76c579e392c
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-11-19 06:34:10 +0000

    task 2 completed +  renamed files

---
+++ task_02/src/bridges_and_cutpoints.cpp
@@ -0,0 +1,76 @@
+#include "bridges_and_cutpoints.hpp"
+
+
+void innerFindBridges(const std::vector<std::vector<int>>& graph, int time, std::vector<int>& tin, std::vector<int>& ret, std::vector<int>& colors, int vertex, int parent, std::set<std::pair<int, int>>& bridges){
+  tin[vertex] = ++time;
+  ret[vertex] = time;
+  colors[vertex] = 1;
+  for (int i = 0; i < graph[vertex].size(); ++i)
+  {
+    if (graph[vertex][i] == parent) continue;
+    if (colors[graph[vertex][i]] > 0)
+      ret[vertex] = std::min(ret[vertex], tin[graph[vertex][i]]);
+    else{
+      innerFindBridges(graph, time, tin, ret, colors, graph[vertex][i], vertex, bridges);
+      ret[vertex] = std::min(ret[vertex], ret[graph[vertex][i]]);
+      if(ret[graph[vertex][i]] > tin[vertex])
+        bridges.insert(std::pair(graph[vertex][i], vertex));
+    }
+  colors[vertex] = 2;
+  }
+}
+
+std::set<std::pair<int, int>> findBridges (const std::vector<std::vector<int>>& graph){
+  int time = 0;
+  std::vector<int> tin(graph.size());
+  std::vector<int> ret(graph.size());
+  std::vector<int> colors(graph.size());
+  std::set<std::pair<int, int>> bridges;
+  if(graph.size() == 0)
+    return bridges;
+  innerFindBridges(graph, time, tin, ret, colors, 0, -1, bridges);
+  return bridges;
+}
+
+
+
+
+
+
+void innerFindCutPoints(const std::vector<std::vector<int>>& graph, int time, std::vector<int>& tin, std::vector<int>& ret,
+                         std::vector<int>& colors, int vertex, int parent, std::set<int>& cut_points){
+  int children = 0;
+  colors[vertex] = 1;
+  tin[vertex] = ++time;
+  ret[vertex] = time;
+  for(size_t i = 0; i < graph[vertex].size(); ++i){
+    if(graph[vertex][i] == parent) continue;
+    if(colors[graph[vertex][i]] != 0)
+      ret[vertex] = std::min(ret[vertex], tin[graph[vertex][i]]);
+    else{
+      innerFindCutPoints(graph, time, tin, ret, colors, graph[vertex][i], vertex, cut_points);
+      ret[vertex] = std::min(ret[vertex], ret[graph[vertex][i]]);
+      if (ret[graph[vertex][i]] >= tin[vertex] && parent != -1)
+				cut_points.insert(vertex);
+			++children;
+    }
+  }
+  if(parent == -1 && children > 1)
+    cut_points.insert(vertex);
+  colors[vertex] = 2;
+}
+
+
+
+std::set<int> findCutPoints(const std::vector<std::vector<int>> &graph){
+  int time = 0;
+  std::vector<int> tin(graph.size());
+  std::vector<int> ret(graph.size());
+  std::vector<int> colors(graph.size());
+  std::set<int> cut_points;
+  if(graph.size() == 0)
+    return cut_points;
+  innerFindCutPoints(graph, time, tin, ret, colors, 0, -1, cut_points);
+  return cut_points;
+}
+
--- task_02/src/test.cpp
+++ task_02/src/test.cpp
@@ -1,8 +1,93 @@
 
 #include <gtest/gtest.h>
+#include <utility>
 
-#include "topology_sort.hpp"
+#include "bridges_and_cutpoints.hpp"
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+TEST(CutPointsAndBridges, SimpleBridges) {
+    std::vector<std::vector<int>> graph = {{1, 2}, {3, 5}, {0, 4, 5}, {1}, {2, 5}, {1, 2, 4, 6}, {5, 7}, {6}};
+    std::set<std::pair<int, int>> bridges = findBridges(graph);
+    std::set<std::pair<int, int>> bridges_ans;
+    bridges_ans.insert(std::pair(3, 1));
+    bridges_ans.insert(std::pair(6, 5));
+    bridges_ans.insert(std::pair(7, 6));
+    ASSERT_EQ(bridges, bridges_ans);
+}
+
+TEST(CutPointsAndBridges, SimpleCutPoints) {
+    std::vector<std::vector<int>> graph = {{1, 2}, {3, 5}, {0, 4, 5}, {1}, {2, 5}, {1, 2, 4, 6}, {5, 7}, {6}};
+    std::set<int> cutpoints = findCutPoints(graph);
+    std::set<int> cutpoints_ans;
+    cutpoints_ans.insert(1);
+    cutpoints_ans.insert(5);
+    cutpoints_ans.insert(6);
+    ASSERT_EQ(cutpoints, cutpoints_ans);
+}
+
+TEST(CutPointsAndBridges, LoopCutpoints) {
+    std::vector<std::vector<int>> graph = {{1, 2, 0}, {3, 5, 1}, {0, 4, 5, 2}, {1, 3}, {2, 5}, {1, 2, 4, 6}, {5, 7}, {6}};
+    std::set<int> cutpoints = findCutPoints(graph);
+    std::set<int> cutpoints_ans;
+    cutpoints_ans.insert(1);
+    cutpoints_ans.insert(5);
+    cutpoints_ans.insert(6);
+    ASSERT_EQ(cutpoints, cutpoints_ans);
+}
+
+TEST(CutPointsAndBridges, LoopBridges) {
+    std::vector<std::vector<int>> graph = {{1, 2, 0}, {3, 5, 1}, {0, 4, 5, 2}, {1}, {2, 5}, {1, 2, 4, 6}, {5, 7}, {6}};
+    std::set<std::pair<int, int>> bridges = findBridges(graph);
+    std::set<std::pair<int, int>> bridges_ans;
+    bridges_ans.insert(std::pair(3, 1));
+    bridges_ans.insert(std::pair(6, 5));
+    bridges_ans.insert(std::pair(7, 6));
+    ASSERT_EQ(bridges, bridges_ans);
+}
+
+TEST(CutPointsAndBridges, EmptyBridges) {
+    std::vector<std::vector<int>> graph = {};
+    std::set<std::pair<int, int>> bridges = findBridges(graph);
+    std::set<std::pair<int, int>> bridges_ans;
+    ASSERT_EQ(bridges, bridges_ans);
+}
+
+TEST(CutPointsAndBridges, EmptyCutpoints) {
+    std::vector<std::vector<int>> graph = {};
+    std::set<int> cutpoints = findCutPoints(graph);
+    std::set<int> cutpoints_ans;
+    ASSERT_EQ(cutpoints, cutpoints_ans);
+}
+
+TEST(CutPointsAndBridges, NoCutpoints) {
+    std::vector<std::vector<int>> graph = {{1, 4, 3}, {0, 4, 2}, {1, 4, 3}, {2, 4, 0}, {0, 1, 2, 3}};
+    std::set<int> cutpoints = findCutPoints(graph);
+    std::set<int> cutpoints_ans;
+    ASSERT_EQ(cutpoints, cutpoints_ans);
+}
+
+TEST(CutPointsAndBridges, NoBridges) {
+    std::vector<std::vector<int>> graph = {{1, 4, 3}, {0, 4, 2}, {1, 4, 3}, {2, 4, 0}, {0, 1, 2, 3}};
+    std::set<std::pair<int, int>> bridges = findBridges(graph);
+    std::set<std::pair<int, int>> bridges_ans;
+    ASSERT_EQ(bridges, bridges_ans);
+}
+
+TEST(CutPointsAndBridges, ComplexBridges) {
+    std::vector<std::vector<int>> graph = {{1, 2}, {0, 2}, {1, 0, 3}, {2, 4, 5}, {3, 5}, {3, 4, 6}, {5, 7, 8}, {6, 8}, {7, 6}};
+    std::set<std::pair<int, int>> bridges = findBridges(graph);
+    std::set<std::pair<int, int>> bridges_ans;
+    bridges_ans.insert(std::pair(3, 2));
+    bridges_ans.insert(std::pair(6, 5));
+    ASSERT_EQ(bridges, bridges_ans);
+}
+
+TEST(CutPointsAndBridges, ComplexCutPoints) {
+    std::vector<std::vector<int>> graph = {{1, 2}, {0, 2}, {1, 0, 3}, {2, 4, 5}, {3, 5}, {3, 4, 6}, {5, 7, 8}, {6, 8}, {7, 6}};
+    std::set<int> cutpoints = findCutPoints(graph);
+    std::set<int> cutpoints_ans;
+    cutpoints_ans.insert(2);
+    cutpoints_ans.insert(3);
+    cutpoints_ans.insert(5);
+    cutpoints_ans.insert(6);
+    ASSERT_EQ(cutpoints, cutpoints_ans);
 }
