9a5f559846d4ecb9a55faabb1d3523cd78de05d5
VariPor <116594957+VariPor@users.noreply.github.com>
2023-11-25 10:24:26 +0000

    bridge_search

--- task_02/src/search.cpp
+++ task_02/src/search.cpp
@@ -1,34 +1,47 @@
 #include "search.hpp"
 
-void dfs(char v, char parent, std::size_t& timer, std::vector<bool>& visited, 
-                std::vector<std::size_t>& t_in, std::vector<std::size_t>& t_up, const Graph& graph,
-                std::vector<edge>& bridges) {
-    visited[v] = true;
-    t_in[v] = timer;
-    t_up[v] = timer;
-    timer += 1;
-    for (std::size_t i = 0; i < graph.at(v).size(); ++i) {
-        char destination = graph.at(v).at(i);
-        if (destination == parent) continue;
-        if (visited.at(destination))
-            t_up[v] = ((t_up.at(v) > t_in.at(v)) ? t_in.at(v) : t_up.at(v));
-        else {
-            dfs(destination, v, timer, visited, t_in, t_up, graph, bridges);
-            t_up[v] = ((t_up.at(v) > t_up.at(destination)) ? t_up.at(destination) : t_up.at(v));
-            if (t_up.at(destination) > t_in.at(v)) 
-                bridges.push_back(edge(v, destination));
-        }
+void dfs(char v, char parent, std::size_t& timer, std::vector<bool>& visited,
+         std::vector<std::size_t>& t_in, std::vector<std::size_t>& t_up,
+         const Graph& graph, std::vector<edge>& bridges) {
+  visited[v] = true;
+  t_in[v] = timer;
+  t_up[v] = timer;
+  timer += 1;
+  for (std::size_t i = 0; i < graph.at(v).size(); ++i) {
+    char destination = graph.at(v).at(i);
+    if (destination == parent) continue;
+    if (visited.at(destination))
+      t_up[v] = ((t_up.at(v) > t_in.at(destination)) ? t_in.at(destination)
+                                                     : t_up.at(v));
+    else {
+      dfs(destination, v, timer, visited, t_in, t_up, graph, bridges);
+      t_up[v] = ((t_up.at(v) > t_up.at(destination)) ? t_up.at(destination)
+                                                     : t_up.at(v));
+      if (t_up.at(destination) > t_in.at(v))
+        bridges.push_back(edge(v, destination));
     }
+  }
+}
+
+Graph prepare(const Graph& graph) {
+  Graph new_graph(graph.size());
+  for (size_t i = 0; i < graph.size(); ++i)
+    for (size_t j = 0; j < graph.at(i).size(); ++j) {
+      new_graph[i].push_back(graph.at(i).at(j));
+      new_graph[graph.at(i).at(j)].push_back(i);
+    }
+  return new_graph;
 }
 
 std::vector<edge> bridge_search(const Graph& graph) {
-    std::vector<bool> visited(graph.size(), false);
-    std::size_t timer = 0;
-    std::vector<std::size_t> t_in(graph.size());
-    std::vector<std::size_t> t_up(graph.size());
-    std::vector<edge> bridges;
-    for (std::size_t i = 0; i < graph.size(); ++i)
-        if (!visited.at(i))
-            dfs(i, -1, timer, visited, t_in, t_up, graph, bridges);
-    return bridges;
+  auto new_graph = prepare(graph);
+  std::vector<bool> visited(new_graph.size(), false);
+  std::vector<std::size_t> t_in(new_graph.size());
+  std::vector<std::size_t> t_up(new_graph.size());
+  std::size_t timer = 0;
+  std::vector<edge> bridges;
+  for (std::size_t i = 0; i < new_graph.size(); ++i)
+    if (!visited.at(i))
+      dfs(i, -1, timer, visited, t_in, t_up, new_graph, bridges);
+  return bridges;
 }
--- task_02/src/test.cpp
+++ task_02/src/test.cpp
@@ -7,3 +7,26 @@ TEST(BridgeSearch, Simple) {
   Graph graph{{1}, {0}};
   ASSERT_EQ(bridge_search(graph), std::vector<edge>{edge(0, 1)});
 }
+
+TEST(BridgeSearch, WithoutBridges) {
+  Graph graph{{}, {}, {3}, {1}, {0, 1}, {0, 2}};
+  ASSERT_EQ(bridge_search(graph), std::vector<edge>{});
+}
+
+TEST(BridgeSearch, ThreeBridges) {
+  Graph graph{{8}, {2, 3, 4}, {4}, {4}, {5}, {6, 7, 8}, {7}, {}, {}};
+  std::vector<edge> result{edge(4, 5), edge(5, 8), edge(0, 8)};
+  ASSERT_EQ(bridge_search(graph), result);
+}
+
+TEST(BridgeSearch, TwoBridges) {
+  Graph graph{{8}, {2, 3, 4}, {4}, {4}, {5}, {6, 7, 8}, {7}, {8}, {}};
+  std::vector<edge> result{edge(4, 5), edge(0, 8)};
+  ASSERT_EQ(bridge_search(graph), result);
+}
+
+TEST(BridgeSearch, AllBridges) {
+  Graph graph{{1}, {2}, {3}, {}};
+  std::vector<edge> result{edge(2, 3), edge(1, 2), edge(0, 1)};
+  ASSERT_EQ(bridge_search(graph), result);
+}
