0cc11870c8e91282f422739ab4d35062a912d83d
G00sya <118297271+G00sya@users.noreply.github.com>
2023-11-25 08:58:36 +0000

    added first task without tests

--- task_01/src/topology_sort.cpp
+++ task_01/src/topology_sort.cpp
@@ -1,1 +1,59 @@
 #include "topology_sort.hpp"
+
+void Graph::TS(int curr_node)
+{
+
+    if (node_colors_[curr_node] != NodeColor::White)
+        return;
+
+    node_colors_[curr_node] = NodeColor::Grey;
+    if (!adjacency_list_[curr_node].empty())
+        for (auto iter = adjacency_list_[curr_node].begin(); iter != adjacency_list_[curr_node].end(); ++iter)
+        {
+            if (node_colors_[*iter] != NodeColor::Grey || node_colors_[*iter] != NodeColor::Black)
+                TS(*iter);
+        }
+
+    order.push(curr_node);
+    node_colors_[curr_node] = NodeColor::Black;
+}
+
+std::vector<int> Graph::Sort()
+{
+    for(std::map<int, std::vector<int>>::iterator iter = adjacency_list_.begin(); iter != adjacency_list_.end(); ++iter)
+    {
+        int key =  iter->first;
+        TS(key);
+    }
+
+    std::vector<int> answer;
+    for (int i = 0; i < node_count_; i++)
+    {
+        int temporary = order.top();
+        answer.push_back(temporary);
+        if (!order.empty())
+            order.pop();
+        else
+            break;
+    }
+
+    return answer;
+}
+
+
+void Graph::Print_graph()
+{
+    for(std::map<int, std::vector<int>>::iterator iter = adjacency_list_.begin(); iter != adjacency_list_.end(); ++iter)
+    {
+        int key =  iter->first;
+        std::vector<int> vec = iter->second;
+        std::cout<<key<<": ";
+
+        for (auto viter = vec.begin(); viter != vec.end(); viter++)
+        {
+            std::cout<<*viter<<' ';
+        }
+
+        std::cout<<"\n";
+    }
+}
--- task_01/src/topology_sort.hpp
+++ task_01/src/topology_sort.hpp
@@ -1,1 +1,36 @@
 #pragma once
+
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <map>
+
+enum class NodeColor
+{
+    White,
+    Grey,
+    Black
+};
+
+class Graph
+{
+private:
+    int node_count_;
+    std::map<int, std::vector<int>> adjacency_list_;
+    std::vector<NodeColor> node_colors_;
+    std::stack<int> order;
+
+public:
+    Graph(int node_count, std::map<int, std::vector<int>> adjacency)
+        :adjacency_list_(adjacency),  node_count_(node_count)
+    {
+        node_colors_.resize(node_count_);
+        for (int i = 0; i < node_count; i++)
+            node_colors_[i] = NodeColor::White;
+
+    }
+
+    void TS(int curr_node);
+    std::vector<int> Sort();
+    void Print_graph();
+};
