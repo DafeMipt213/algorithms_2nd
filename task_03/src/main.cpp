5b97298b0caeca0d8c104e40742175c92688843f
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-11-27 23:15:49 +0000

    added task 3

--- task_03/src/main.cpp
+++ task_03/src/main.cpp
@@ -1,3 +1,13 @@
+#include "Jonson.hpp"
 #include <iostream>
 
-int main() { return 0; }
+
+int main() { 
+    std::vector<std::vector<std::pair<int, int>>> graph{{{5, 1}}, {{0, 1}}, {{1, 1}, {3, 1}}, {{4, 1}}, {{5, 1}}, {{2, 3}}};
+    std::vector<std::vector<int>> d = jonson(graph);
+    for(int i = 0; i < d.size(); ++i){
+        for(int j = 0; j < d.size(); ++j)
+            std::cout << d[i][j] << " ";
+        std::cout << '\n';
+    }
+}
