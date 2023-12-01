b2d1c2de82d4819ace9c70e695f899cb17942a46
ShatunovEvgeniy <118297183+ShatunovEvgeniy@users.noreply.github.com>
2023-12-01 17:12:25 +0000

    feat(sort): Add topology sort

--- task_01/src/topology_sort.cpp
+++ task_01/src/topology_sort.cpp
@@ -2,19 +2,61 @@
 
 #include "topology_sort.hpp"
 
-void Graph::add_element(Vertex* parent) 
+void Graph::AddRoot(Vertex* root) 
 {
-    Vertex v = Vertex(this->_cur_number, parent);
-    this->_cur_number++;
+    this->_roots.push_back(root);
 }
 
-std::vector<Vertex*> Graph::topology_sort()
+void Graph::AddElement(int number, std::vector<Vertex*> parents) 
+{
+    if (parents.empty())
+        throw std::runtime_error("Vertex has no parents");
+
+    Vertex v = Vertex(number, parents);
+}
+
+std::vector<int> Graph::TopologySort()
 {
     if (this->_roots.empty())
         throw std::runtime_error("Graph is empty. Nothing to sort");
 
-    // for(Vertex* root : this->_roots)
-    // {
-    //     root.
-    // }
+    std::vector<int> result;
+    for(Vertex* root : this->_roots)
+    {
+        root->ChangeColour(GRAY);
+        std::vector<int> current_sorting = this->_TopologySort(root);
+        root->ChangeColour(BLACK);
+        result.push_back(root->GetNumber());
+        result.insert(result.end(), current_sorting.begin(), current_sorting.end());
+    }
+    return result;
+}
+
+std::vector<int> Graph::_TopologySort(Vertex* root)
+{
+    std::vector<int> result;
+    
+    for (Vertex* vertex : root->GetChildren())
+        {
+            Colour vertex_colour = vertex->GetColour(); 
+            if(vertex_colour == WHITE)
+            {
+                vertex->ChangeColour(GRAY);
+                std::vector<int> current_sorting = this->_TopologySort(vertex);
+                vertex->ChangeColour(BLACK);
+                result.push_back(vertex->GetNumber());
+                result.insert(result.end(), current_sorting.begin(), current_sorting.end());
+            }
+            else if (vertex_colour == BLACK)
+                continue;
+            else if (vertex_colour == GRAY)
+                throw std::runtime_error("There is a cycle in the graph");
+        }
+    return result;
+}
+
+void AddEdge(Vertex* root, Vertex* child)
+{
+    child->AddParent(root);
+    root->AddChild(child);
 }
--- task_01/src/topology_sort.hpp
+++ task_01/src/topology_sort.hpp
@@ -23,18 +23,21 @@ class Vertex
     /* Struct of a vertex in a graph */
 
     public:
-        Vertex(size_t number, Vertex* parent = nullptr): 
-            _parent{parent}, _number{number} {}
+        Vertex(int number, std::vector<Vertex*> parents = std::vector<Vertex*>()): 
+            _parents{parents}, _number{number} {}
 
-        Vertex* get_parent() {return _parent; };
-        std::vector<Vertex*> get_children() { return _children; };
+        std::vector<Vertex*> GetParents() {return _parents; }
+        std::vector<Vertex*> GetChildren() { return _children; }
+        Colour GetColour() { return _colour; }
+        int GetNumber() { return _number; }
 
-        void add_child(Vertex* children) { _children.push_back(children); };
-        void change_colour(Colour new_colour) { this->_colour = new_colour; };
+        void AddChild(Vertex* children) { _children.push_back(children); }
+        void ChangeColour(Colour new_colour) { this->_colour = new_colour; }
+        void AddParent(Vertex* parent) { _parents.push_back(parent); }
 
     private:
-        size_t _number;
-        Vertex* _parent = nullptr;
+        int _number;
+        std::vector<Vertex*> _parents;
         std::vector<Vertex*> _children;
         Colour _colour = WHITE; // possbile colours are "white", "gray" and "'black"
 };
@@ -42,14 +45,15 @@ class Vertex
 class Graph
 {
     public:
-        Graph(bool root=false){
-            if (root)
-                this->_roots.push_back(new Vertex(this->_cur_number++));
-        }
+        Graph() = default;
 
-        void add_element(Vertex* parent=nullptr); // add new element to the graph
-        std::vector<Vertex*> topology_sort(); // get sorted vertices
+        void AddRoot(Vertex* root); // add new root to the graph
+        void AddElement(int number, std::vector<Vertex*> parents); // add new element to the graph
+        std::vector<int> TopologySort(); // get sorted vertices
     private:
+        std::vector<int> _TopologySort(Vertex* vertex); // recursion of topology sort for this vertex
         std::vector<Vertex*> _roots; // vector with all roots in graph
-        size_t _cur_number = 1; // number of last added vertex
+      //  size_t _cur_number = 1; // number of last added vertex
 };
+
+void AddEdge(Vertex* root, Vertex* child);
