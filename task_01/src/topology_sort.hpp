feab2417119d3efcdb9be51e362fe56ec0c68997
CHICH <118026077+AldMatvey@users.noreply.github.com>
2023-12-23 08:49:49 +0000

    topology sort: created

--- task_01/src/topology_sort.hpp
+++ task_01/src/topology_sort.hpp
@@ -1,1 +1,19 @@
 #pragma once
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+
+struct Graph
+{
+    std::vector<std::vector<int>> graph_list;
+    std::vector<int> tin;
+    std::vector<int> tout;
+    std::vector<int> colors;
+    std::stack<int> topology_stack;
+    int timer;
+    Graph(std::vector<std::vector<int>> new_graph );
+    void fillStack(int vertex);
+    std::vector<int> topologySort(int vertex);
+    void dfs(int vertex);
+};
