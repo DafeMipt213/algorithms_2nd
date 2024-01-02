231dfca195e60c13970656991e164ba415f7a750
VladimirSpe <75093117+VladimirSpe@users.noreply.github.com>
2024-01-02 16:31:00 +0000

    graph

---
+++ lib/src/graph.cpp
@@ -0,0 +1,17 @@
+#include "graph.hpp"
+
+Graph::Graph(const std::vector<std::vector<size_t>> &adjacency_list)
+    : g{adjacency_list}, color(adjacency_list.size(), Color::White) {}
+
+void Graph::dfs(int v, int p) {
+  tin[v] = timer++;
+  parent[v] = p;
+  color[v] = Color::Gray;
+  for (int to : g[v]) {
+    if (color[to] != Color::White)
+      continue;
+    dfs(to, v);
+  }
+  tout[v] = timer++;
+  color[v] = Color::Black;
+}
---
+++ lib/src/graph.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+enum Color { White, Gray, Black };
+
+class Graph {
+private:
+  vector<vector<size_t>> g;
+  vector<size_t> tin, tout;
+  int timer = 0;
+  vector<Color> color;
+  vector<int> parent;
+public:
+  Graph(const std::vector<std::vector<size_t>>& adjacency_list);
+  void dfs(int v, int p = -1);
+};
