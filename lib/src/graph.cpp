c5f76ddc37ad738557df16b2508caf6313b44515
VladimirSpe <75093117+VladimirSpe@users.noreply.github.com>
2024-01-03 13:26:10 +0000

    first task

--- lib/src/graph.cpp
+++ lib/src/graph.cpp
@@ -1,7 +1,9 @@
 #include "graph.hpp"
 
 Graph::Graph(const std::vector<std::vector<size_t>> &adjacency_list)
-    : g{adjacency_list}, color(adjacency_list.size(), Color::White) {}
+    : g{adjacency_list}, color(adjacency_list.size(), Color::White),
+      parent(adjacency_list.size()), tin(adjacency_list.size()),
+      tout(adjacency_list.size()) {}
 
 void Graph::dfs(int v, int p) {
   tin[v] = timer++;
--- lib/src/graph.hpp
+++ lib/src/graph.hpp
@@ -11,7 +11,10 @@ private:
   int timer = 0;
   vector<Color> color;
   vector<int> parent;
+
 public:
-  Graph(const std::vector<std::vector<size_t>>& adjacency_list);
+  Graph(const std::vector<std::vector<size_t>> &adjacency_list);
   void dfs(int v, int p = -1);
+  vector<size_t> topologicalSort();
 };
+
