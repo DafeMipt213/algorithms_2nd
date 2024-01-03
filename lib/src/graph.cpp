d0b27da8bf0da27d4f251a9d6a0f4c378b252aa4
VladimirSpe <75093117+VladimirSpe@users.noreply.github.com>
2024-01-03 17:57:51 +0000

    second task without tests

--- lib/src/graph.cpp
+++ lib/src/graph.cpp
@@ -1,9 +1,10 @@
 #include "graph.hpp"
 
-Graph::Graph(const std::vector<std::vector<size_t>> &adjacency_list)
+Graph::Graph(const vector<vector<int>> &adjacency_list)
     : g{adjacency_list}, color(adjacency_list.size(), Color::White),
       parent(adjacency_list.size()), tin(adjacency_list.size()),
-      tout(adjacency_list.size()) {}
+      tout(adjacency_list.size()), used(adjacency_list.size(), false),
+      ret(adjacency_list.size()) {}
 
 void Graph::dfs(int v, int p) {
   tin[v] = timer++;
--- lib/src/graph.hpp
+++ lib/src/graph.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <utility>
 #include <vector>
 using namespace std;
 
@@ -6,15 +7,21 @@ enum Color { White, Gray, Black };
 
 class Graph {
 private:
-  vector<vector<size_t>> g;
+  vector<vector<int>> g;
   vector<size_t> tin, tout;
   int timer = 0;
+  vector<pair<size_t, size_t>> vec_bridges;
+  vector<size_t> vec_articulation_points;
+  vector<bool> used;
+  vector<size_t> ret;
   vector<Color> color;
   vector<int> parent;
+  void util_articulation_points_and_bridge_finder(size_t v, int p);
 
 public:
-  Graph(const std::vector<std::vector<size_t>> &adjacency_list);
+  explicit Graph(const vector<vector<int>> &adjacency_list);
   void dfs(int v, int p = -1);
+  vector<size_t> articulation_points_finder(size_t v, int p = -1);
+  vector<pair<size_t, size_t>> bridge_finder(size_t v, int p = -1);
   vector<size_t> topologicalSort();
 };
-
