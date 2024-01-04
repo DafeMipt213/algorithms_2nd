e7408da5aa96afa9040999862a6192769a6729cb
Egor Ivanov <56685477+sibenshtern@users.noreply.github.com>
2024-01-04 10:50:54 +0000

    finish task 3 and write one test

---
+++ task_03/src/johnson.cpp
@@ -0,0 +1,122 @@
+#include "johnson.hpp"
+
+#include <functional>
+#include <queue>
+
+void Graph::AddEdge(int from, int to, long long weight) {
+  if (std::find(vertexes_.begin(), vertexes_.end(), from) == vertexes_.end()) {
+    vertexes_.push_back(from);
+    distances_[from] = LONG_LONG_MAX;
+  }
+  if (std::find(vertexes_.begin(), vertexes_.end(), to) == vertexes_.end()) {
+    vertexes_.push_back(to);
+    distances_[to] = LONG_LONG_MAX;
+  }
+  edges_.push_back({from, to, weight});
+
+  adjacency_map_[from].push_back({to, weight});
+  if (adjacency_map_.find(to) == adjacency_map_.end()) adjacency_map_[to] = {};
+}
+
+void Graph::AddVertex(int vertex) {
+  if (std::find(vertexes_.begin(), vertexes_.end(), vertex) != vertexes_.end())
+    return;
+  vertexes_.push_back(vertex);
+  distances_[vertex] = LONG_LONG_MAX;
+}
+
+void Graph::PrepareGraph_() {
+  size_t vertexes_count = vertexes_.size();
+  for (size_t i = 0; i < vertexes_count; i++) {
+    AddEdge(INT_MIN, vertexes_[i], 0);
+  }
+}
+
+bool Graph::BellmanFord(int vertex) {
+  distances_[vertex] = 0;
+
+  for (int i = 0; i < vertexes_.size(); i++) {
+    for (auto &edge : edges_) {
+      if (distances_[edge.from] != LONG_LONG_MAX &&
+          distances_[edge.from] + edge.weight < distances_[edge.to])
+        distances_[edge.to] = distances_[edge.from] + edge.weight;
+    }
+  }
+
+  for (auto &edge : edges_) {
+    if (distances_[edge.from] != LONG_LONG_MAX &&
+        distances_[edge.from] + edge.weight < distances_[edge.to])
+      is_cycled = true;
+  }
+  return is_cycled;
+}
+
+void Graph::ChangeEdges_() {
+  if (is_cycled) return;
+
+  for (auto &vertex : vertexes_) values_[vertex] = distances_[vertex];
+  for (auto &edge : edges_)
+    if (edge.from != INT_MIN)
+      edge.weight = edge.weight + values_[edge.from] - values_[edge.to];
+
+  size_t additional_edges_index = -1;
+  for (size_t i = 0; i < edges_.size(); i++) {
+    if (edges_[i].from == INT_MIN) {
+      additional_edges_index = i;
+      break;
+    }
+  }
+
+  edges_.erase(edges_.begin() + additional_edges_index + 1, edges_.end());
+
+  if (vertexes_.back() == INT_MIN)
+    vertexes_.erase(vertexes_.end() - 1);
+  else
+    for (size_t i = 0; i < vertexes_.size(); i++) {
+      if (vertexes_[i] == INT_MIN) {
+        vertexes_.erase(vertexes_.begin() + i);
+        break;
+      }
+    }
+}
+
+std::map<int, long long> Graph::Dijkstra(int root) {
+  std::map<int, long long> distances;
+  for (auto &vertex : vertexes_) distances[vertex] = LONG_LONG_MAX;
+
+  distances[root] = 0;
+
+  std::priority_queue<std::pair<long long, int>,
+                      std::vector<std::pair<long long, int>>,
+                      std::greater<std::pair<long long, int>>>
+      queue;
+  queue.push({0, root});
+  while (!queue.empty()) {
+    auto [current_distance, vertex] = queue.top();
+    queue.pop();
+
+    if (current_distance > distances[vertex]) continue;
+    for (auto &[next_vertex, weight] : adjacency_map_[vertex]) {
+      if (distances[next_vertex] > distances[vertex] + weight) {
+        distances[next_vertex] = distances[vertex] + weight;
+        queue.push({distances[next_vertex], next_vertex});
+      }
+    }
+  }
+  return distances;
+}
+
+std::map<int, std::map<int, long long>> Graph::Johnson() {
+  std::map<int, std::map<int, long long>> result;
+
+  PrepareGraph_();
+  BellmanFord(INT_MIN);
+  if (is_cycled) return {};
+
+  ChangeEdges_();
+  for (auto &vertex : vertexes_) {
+    auto distances = Dijkstra(vertex);
+    result[vertex] = distances;
+  }
+  return result;
+}
--- task_03/src/test.cpp
+++ task_03/src/test.cpp
@@ -1,6 +1,25 @@
-
 #include <gtest/gtest.h>
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+#include <iostream>
+
+#include "johnson.hpp"
+
+TEST(JohnsonAlgorithm, GraphFromHabr) {
+  Graph graph;
+  graph.AddEdge(1, 2, -2);
+  graph.AddEdge(1, 3, 7);
+  graph.AddEdge(1, 4, 5);
+  graph.AddEdge(2, 4, 6);
+  graph.AddEdge(2, 3, 8);
+  graph.AddEdge(3, 4, -4);
+  graph.AddEdge(3, 2, 3);
+  graph.AddEdge(4, 1, -1);
+
+  std::map<int, std::map<int, long long>> result = {
+      {1, {{1, 0}, {2, -2}, {3, 6}, {4, 2}}},
+      {2, {{1, 3}, {2, 0}, {3, 8}, {4, 4}}},
+      {3, {{1, -5}, {2, -7}, {3, 0}, {4, -4}}},
+      {4, {{1, -1}, {2, -3}, {3, 5}, {4, 0}}}};
+
+  ASSERT_EQ(graph.Johnson(), result);
 }
