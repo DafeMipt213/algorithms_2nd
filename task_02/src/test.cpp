63bcd9cdbfc811238af40bad59be07c1b1cf364e
Egor Ivanov <56685477+sibenshtern@users.noreply.github.com>
2024-01-07 07:22:01 +0000

    Add tests for task 2

--- task_02/src/test.cpp
+++ task_02/src/test.cpp
@@ -65,3 +65,72 @@ TEST(Task, FullConnectedGraph) {
   ASSERT_EQ(graph.GetConnections(), connections);
   ASSERT_EQ(graph.GetRouters(), routers);
 }
+
+TEST(Task, AnotherGraph) {
+  Graph graph;
+  graph.AddEdge(1, 2);
+  graph.AddEdge(1, 3);
+  graph.AddEdge(2, 3);
+  graph.AddEdge(3, 4);
+  graph.CalculateSolution();
+  std::vector<std::pair<int, int>> connections{{3, 4}};
+  std::vector<int> routers{3};
+
+  ASSERT_EQ(graph.GetConnections(), connections);
+  ASSERT_EQ(graph.GetRouters(), routers);
+}
+
+TEST(Task, YetAnotherGraph) {
+  Graph graph;
+  graph.AddEdge(1, 2);
+  graph.AddEdge(1, 3);
+  graph.AddEdge(1, 4);
+  graph.AddEdge(2, 5);
+  graph.AddEdge(3, 6);
+  graph.AddEdge(4, 7);
+  graph.CalculateSolution();
+  std::vector<std::pair<int, int>> connections{{2, 5}, {1, 2}, {3, 6},
+                                               {1, 3}, {4, 7}, {1, 4}};
+  std::vector<int> routers{2, 3, 4, 1};
+
+  ASSERT_EQ(graph.GetConnections(), connections);
+  ASSERT_EQ(graph.GetRouters(), routers);
+}
+
+TEST(Task, AnotherGraphTest) {
+  Graph graph;
+  graph.AddEdge(5, 6);
+  graph.AddEdge(5, 7);
+  graph.AddEdge(6, 8);
+  graph.AddEdge(7, 9);
+  graph.AddEdge(8, 10);
+  graph.CalculateSolution();
+  std::vector<std::pair<int, int>> connections{
+      {8, 10}, {6, 8}, {5, 6}, {7, 9}, {5, 7}};
+  std::vector<int> routers{8, 6, 7, 5};
+
+  ASSERT_EQ(graph.GetConnections(), connections);
+  ASSERT_EQ(graph.GetRouters(), routers);
+}
+
+TEST(Task, HandGraph) {
+  Graph graph;
+  graph.SetAdjacencyMap({{0, {1, 6, 2}},
+                         {1, {0, 3, 5, 6}},
+                         {2, {0, 6, 7}},
+                         {3, {4, 5, 1}},
+                         {4, {3, 5}},
+                         {5, {3, 4, 1}},
+                         {6, {0, 1, 2, 7, 9}},
+                         {7, {2, 6, 11}},
+                         {8, {9, 10}},
+                         {9, {6, 8, 10, 11}},
+                         {10, {8, 9, 11}},
+                         {11, {7, 9, 10}}});
+  graph.CalculateSolution();
+  std::vector<std::pair<int, int>> connections{};
+  std::vector<int> routers{1};
+
+  ASSERT_EQ(graph.GetConnections(), connections);
+  ASSERT_EQ(graph.GetRouters(), routers);
+}
