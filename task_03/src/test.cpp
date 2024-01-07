f307078f15374480632ecfef4cfe28ad5f029211
Egor Ivanov <56685477+sibenshtern@users.noreply.github.com>
2024-01-07 07:22:19 +0000

    Add tests for task 3

--- task_03/src/test.cpp
+++ task_03/src/test.cpp
@@ -23,3 +23,32 @@ TEST(JohnsonAlgorithm, GraphFromHabr) {
 
   ASSERT_EQ(graph.Johnson(), result);
 }
+
+TEST(JohnsonAlgorithm, SimpleGraph) {
+  Graph graph;
+  graph.AddEdge(1, 2, 3);
+  graph.AddEdge(2, 3, 4);
+  graph.AddEdge(3, 1, -6);
+
+  std::map<int, std::map<int, long long>> result = {
+      {1, {{1, 0}, {2, 3}, {3, 7}}},
+      {2, {{1, -2}, {2, 0}, {3, 4}}},
+      {3, {{1, -6}, {2, -3}, {3, 0}}}};
+
+  ASSERT_EQ(graph.Johnson(), result);
+}
+
+TEST(JohnsonAlgorithm, MediumGraph) {
+  Graph graph;
+  for (int i = 1; i <= 4; ++i) {
+    graph.AddEdge(i, i % 4 + 1, i);
+  }
+
+  std::map<int, std::map<int, long long>> result = {
+      {1, {{1, 0}, {2, 1}, {3, 3}, {4, 6}}},
+      {2, {{1, 9}, {2, 0}, {3, 2}, {4, 5}}},
+      {3, {{1, 7}, {2, 8}, {3, 0}, {4, 3}}},
+      {4, {{1, 4}, {2, 5}, {3, 7}, {4, 0}}}};
+
+  ASSERT_EQ(graph.Johnson(), result);
+}
