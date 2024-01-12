577c279961559f5e33e0907b7fb4708368e7b0af
CHICH <118026077+AldMatvey@users.noreply.github.com>
2024-01-12 21:13:45 +0000

    clang fixed

--- task_01/src/test.cpp
+++ task_01/src/test.cpp
@@ -16,13 +16,16 @@ TEST(TopologySort, Cycle) {
   ASSERT_EQ(graph.topologySort(0), res);
 }
 TEST(TopologySort, Hard) {
-  std::vector<std::vector<int>> input = {{1, 2}, {2, 3, 4, 5, 6, 8}, {3}, {}, {5}, {}, {7}, {1}, {0}};
+  std::vector<std::vector<int>> input = {
+      {1, 2}, {2, 3, 4, 5, 6, 8}, {3}, {}, {5}, {}, {7}, {1}, {0}};
   Graph graph(input);
   std::vector<int> res = {0, 1, 8, 6, 7, 4, 5, 2, 3};
   ASSERT_EQ(graph.topologySort(0), res);
 }
 TEST(TopologySort, Comlex) {
-  std::vector<std::vector<int>> input = {{1, 2}, {3}, {3, 4}, {1, 4}, {0}, {}, {7, 8}, {10}, {9, 10}, {6, 11}, {7}, {}, {9, 10, 11}};
+  std::vector<std::vector<int>> input = {
+      {1, 2}, {3},     {3, 4},  {1, 4}, {0}, {},         {7, 8},
+      {10},   {9, 10}, {6, 11}, {7},    {},  {9, 10, 11}};
   Graph graph(input);
   std::vector<int> res = {5, 12, 9, 11, 6, 8, 7, 10, 0, 2, 1, 3, 4};
   ASSERT_EQ(graph.topologySort(0), res);
@@ -36,14 +39,19 @@ TEST(TopologySort, Empty) {
 }
 
 TEST(TopologySort, FullGraphLoops) {
-  std::vector<std::vector<int>> input = {{0, 1, 2, 3, 4}, {0, 1, 2, 3, 4}, {0, 1, 2, 3, 4}, {0, 1, 2, 3, 4}, {0, 1, 2, 3, 4}};
+  std::vector<std::vector<int>> input = {{0, 1, 2, 3, 4},
+                                         {0, 1, 2, 3, 4},
+                                         {0, 1, 2, 3, 4},
+                                         {0, 1, 2, 3, 4},
+                                         {0, 1, 2, 3, 4}};
   Graph graph(input);
   std::vector<int> res = {0, 1, 2, 3, 4};
   ASSERT_EQ(graph.topologySort(0), res);
 }
 
 TEST(TopologySort, FullGraphNoLoops) {
-  std::vector<std::vector<int>> input = {{1, 2, 3, 4}, {0, 2, 3, 4}, {0, 1, 3, 4}, {0, 1, 2, 4}, {0, 1, 2, 3}};
+  std::vector<std::vector<int>> input = {
+      {1, 2, 3, 4}, {0, 2, 3, 4}, {0, 1, 3, 4}, {0, 1, 2, 4}, {0, 1, 2, 3}};
   Graph graph(input);
   std::vector<int> res = {0, 1, 2, 3, 4};
   ASSERT_EQ(graph.topologySort(0), res);
@@ -57,14 +65,17 @@ TEST(TopologySort, NotConnected_2components) {
 }
 
 TEST(TopologySort, NotConnected_3components) {
-  std::vector<std::vector<int>> input = {{1, 2, 3}, {0}, {1}, {2}, {}, {4}, {7}, {6}};
+  std::vector<std::vector<int>> input = {{1, 2, 3}, {0}, {1}, {2},
+                                         {},        {4}, {7}, {6}};
   Graph graph(input);
   std::vector<int> res = {5, 4, 7, 6, 0, 3, 2, 1};
   ASSERT_EQ(graph.topologySort(0), res);
 }
 
 TEST(TopologySort, HugeGraph) {
-  std::vector<std::vector<int>> input = {{1, 4}, {0, 2, 3, 4}, {3}, {7}, {3}, {4, 6, 10, 11, 12}, {12}, {8, 5}, {9}, {7, 10}, {11}, {12}, {}};
+  std::vector<std::vector<int>> input = {
+      {1, 4}, {0, 2, 3, 4}, {3},  {7},  {3}, {4, 6, 10, 11, 12}, {12}, {8, 5},
+      {9},    {7, 10},      {11}, {12}, {}};
   Graph graph(input);
   std::vector<int> res = {0, 1, 2, 3, 7, 5, 6, 4, 8, 9, 10, 11, 12};
   ASSERT_EQ(graph.topologySort(0), res);
