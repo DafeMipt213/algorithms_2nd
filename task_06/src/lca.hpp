e13db0e877db4b647b47aa5326fc721e520192d3
Egor Ivanov <56685477+sibenshtern@users.noreply.github.com>
2024-01-12 09:42:24 +0000

    rewrite LCA to templates

--- task_06/src/lca.hpp
+++ task_06/src/lca.hpp
@@ -1,14 +1,16 @@
 #pragma once
 
+#include <map>
 #include <vector>
 
 // Запрос - О(1), препроцессинг - О(n)
+template <typename T = int>
 class Solution {
-  std::vector<int> heights_;
-  std::vector<int> eulerian_tour_;
-  std::vector<int> eulerian_tour_position_;
+  std::map<T, int> heights_;
+  std::vector<T> eulerian_tour_;
+  std::map<T, int> eulerian_tour_position_;
 
-  std::vector<std::vector<int>> adjacency_list_;
+  std::map<T, std::vector<T>> adjacency_list_;
 
   std::vector<std::vector<int>> sparse_table_;
   std::vector<int> block_hashes_;
@@ -23,12 +25,130 @@ class Solution {
     return heights_[eulerian_tour_[i]] < heights_[eulerian_tour_[j]] ? i : j;
   }
 
-  void DFS(int vertex, int parent, int current_height);
+  void DFS(T vertex, T parent, int current_height, bool is_root = false);
   void BuildLCA();
   int LCAInBlock(int b, int l, int r);
 
  public:
   Solution(std::vector<int> &data);
-  Solution(std::vector<std::vector<int>> &data, int root);
-  int LCA(int v, int u);
+  Solution(std::map<T, std::vector<T>> &data, T root);
+  int LCA(T v, T u);
 };
+
+template <typename T>
+Solution<T>::Solution(std::map<T, std::vector<T>> &data, T root) {
+  int size = data.size();
+  for (auto &[vertex, vertexes] : data) {
+    eulerian_tour_position_[vertex] = -1;
+    heights_[vertex] = 0;
+  }
+  eulerian_tour_.reserve(2 * size);
+
+  adjacency_list_ = data;
+
+  DFS(root, root, 0, true);
+  BuildLCA();
+}
+
+template <typename T>
+void Solution<T>::DFS(T vertex, T parent, int current_height, bool is_root) {
+  if (adjacency_list_.find(vertex) == adjacency_list_.end()) return;
+
+  eulerian_tour_position_[vertex] = (int)eulerian_tour_.size();
+  eulerian_tour_.push_back(vertex);
+  heights_[vertex] = current_height;
+
+  for (int u : adjacency_list_[vertex]) {
+    if (u == parent && !is_root) continue;
+    DFS(u, vertex, current_height + 1);
+    eulerian_tour_.push_back(vertex);
+  }
+}
+
+template <typename T>
+void Solution<T>::BuildLCA() {
+  int m = (int)eulerian_tour_.size();
+
+  log_2_.reserve(m + 1);
+  log_2_.push_back(-1);
+
+  for (int i = 1; i <= m; i++) log_2_.push_back(log_2_[i / 2] + 1);
+
+  block_size_ = std::max(1, log_2_[m] / 2);
+  block_count_ = (m + block_size_ - 1) / block_size_;
+
+  sparse_table_.assign(block_count_,
+                       std::vector<int>(log_2_[block_count_] + 1));
+  for (int i = 0, j = 0, b = 0; i < m; i++, j++) {
+    if (j == block_size_) j = 0, ++b;
+    if (j == 0 || MinHeight(i, sparse_table_[b][0]) == i)
+      sparse_table_[b][0] = i;
+  }
+  for (int l = 1; l <= log_2_[block_count_]; l++) {
+    for (int i = 0; i < block_count_; i++) {
+      int ni = i + (1 << (l - 1));
+      if (ni >= block_count_)
+        sparse_table_[i][l] = sparse_table_[i][l - 1];
+      else
+        sparse_table_[i][l] =
+            MinHeight(sparse_table_[i][l - 1], sparse_table_[ni][l - 1]);
+    }
+  }
+
+  block_hashes_.assign(block_count_, 0);
+  for (int i = 0, j = 0, b = 0; i < m; i++, j++) {
+    if (j == block_size_) j = 0, ++b;
+    if (j > 0 && (i >= m || MinHeight(i - 1, i) == i - 1))
+      block_hashes_[b] += 1 << (j - 1);
+  }
+
+  int possibilities = 1 << (block_size_ - 1);
+  block_rmq_.resize(possibilities);
+  for (int b = 0; b < block_count_; b++) {
+    int mask = block_hashes_[b];
+    if (!block_rmq_[mask].empty()) continue;
+    block_rmq_[mask].assign(block_size_, std::vector<int>(block_size_));
+    for (int l = 0; l < block_size_; l++) {
+      block_rmq_[mask][l][l] = l;
+      for (int r = l + 1; r < block_size_; r++) {
+        block_rmq_[mask][l][r] = block_rmq_[mask][l][r - 1];
+        if (b * block_size_ + r < m)
+          block_rmq_[mask][l][r] =
+              MinHeight(b * block_size_ + block_rmq_[mask][l][r],
+                        b * block_size_ + r) -
+              b * block_size_;
+      }
+    }
+  }
+}
+
+template <typename T>
+int Solution<T>::LCAInBlock(int b, int l, int r) {
+  return block_rmq_[block_hashes_[b]][l][r] + b * block_size_;
+}
+
+template <typename T>
+int Solution<T>::LCA(T v, T u) {
+  if (adjacency_list_.find(v) == adjacency_list_.end() ||
+      adjacency_list_.find(u) == adjacency_list_.end())
+    return -1;
+  int l = eulerian_tour_position_[v];
+  int r = eulerian_tour_position_[u];
+  if (l > r) std::swap(l, r);
+  int block_l = l / block_size_;
+  int block_r = r / block_size_;
+  if (block_l == block_r)
+    return eulerian_tour_[LCAInBlock(block_l, l % block_size_,
+                                     r % block_size_)];
+  int answer_l = LCAInBlock(block_l, l % block_size_, block_size_ - 1);
+  int answer_r = LCAInBlock(block_r, 0, r % block_size_);
+  int answer = MinHeight(answer_l, answer_r);
+
+  if (block_l + 1 < block_r) {
+    int l = log_2_[block_r - block_l - 1];
+    int answer3 = sparse_table_[block_l + 1][l];
+    int answer4 = sparse_table_[block_r - (1 << l)][l];
+    answer = MinHeight(answer, MinHeight(answer3, answer4));
+  }
+  return eulerian_tour_[answer];
+}
--- task_06/src/test.cpp
+++ task_06/src/test.cpp
@@ -4,39 +4,45 @@
 #include "lca.hpp"
 
 TEST(LCA, Simple) {
-  std::vector<std::vector<int>> graph{{2, 3}, {2}, {0, 1},    {0, 4}, {3, 7},
-                                      {6, 7}, {5}, {4, 5, 8}, {8}};
+  std::map<int, std::vector<int>> graph{
+      {0, {2, 3}}, {1, {2}}, {2, {0, 1}},    {3, {0, 4}}, {4, {3, 7}},
+      {5, {6, 7}}, {6, {5}}, {7, {4, 5, 8}}, {8, {8}}};
   Solution solution(graph, 4);
   ASSERT_EQ(solution.LCA(6, 8), 7);
   ASSERT_EQ(solution.LCA(1, 6), 4);
 }
 
 TEST(LCA, Complex) {
-  std::vector<std::vector<int>> graph{
-      {1, 2, 3},    {0, 4, 5}, {0, 6, 7},   {0, 8, 9}, {1, 10}, {1, 11, 12},
-      {2, 13},      {2, 14},   {3},         {9},       {4},     {5, 15, 16},
-      {5},          {6},       {7, 17, 18}, {11},      {11},    {14},
-      {14, 19, 20}, {18},      {18}};
+  std::map<int, std::vector<int>> graph{
+      {0, {1, 2, 3}}, {1, {0, 4, 5}},   {2, {0, 6, 7}},     {3, {0, 8, 9}},
+      {4, {1, 10}},   {5, {1, 11, 12}}, {6, {2, 13}},       {7, {2, 14}},
+      {8, {3}},       {9, {9}},         {10, {4}},          {11, {5, 15, 16}},
+      {12, {5}},      {13, {6}},        {14, {7, 17, 18}},  {15, {11}},
+      {16, {11}},     {17, {14}},       {18, {14, 19, 20}}, {19, {18}},
+      {20, {18}}};
   Solution solution(graph, 0);
   ASSERT_EQ(solution.LCA(15, 12), 5);
   ASSERT_EQ(solution.LCA(13, 17), 2);
 }
 
 TEST(LCA, AnotherComplexTest) {
-  std::vector<std::vector<int>> graph{
-      {1, 2, 3},    {0, 4, 5}, {0, 6, 7},   {0, 8, 9}, {1, 10}, {1, 11, 12},
-      {2, 13},      {2, 14},   {3},         {3},       {4},     {5, 15, 16},
-      {5},          {6},       {7, 17, 18}, {11},      {11},    {14},
-      {14, 19, 20}, {18},      {18}};
+  std::map<int, std::vector<int>> graph{
+      {0, {1, 2, 3}}, {1, {0, 4, 5}},   {2, {0, 6, 7}},     {3, {0, 8, 9}},
+      {4, {1, 10}},   {5, {1, 11, 12}}, {6, {2, 13}},       {7, {2, 14}},
+      {8, {3}},       {9, {3}},         {10, {4}},          {11, {5, 15, 16}},
+      {12, {5}},      {13, {6}},        {14, {7, 17, 18}},  {15, {11}},
+      {16, {11}},     {17, {14}},       {18, {14, 19, 20}}, {19, {18}},
+      {20, {18}}};
   Solution solution(graph, 0);
   ASSERT_EQ(solution.LCA(10, 12), 1);
   ASSERT_EQ(solution.LCA(13, 19), 2);
 }
 
 TEST(LCA, DifferentGraphStructure) {
-  std::vector<std::vector<int>> graph{{1, 2},    {0, 3, 4}, {0, 5, 6},  {1},
-                                      {1, 7, 8}, {2},       {2, 9, 10}, {4},
-                                      {4},       {6},       {6}};
+  std::map<int, std::vector<int>> graph{
+      {0, {1, 2}},    {1, {0, 3, 4}}, {2, {0, 5, 6}},  {3, {1}},
+      {4, {1, 7, 8}}, {5, {2}},       {6, {2, 9, 10}}, {7, {4}},
+      {8, {4}},       {9, {6}},       {10, {6}}};
   Solution solution(graph, 0);
   ASSERT_EQ(solution.LCA(7, 8), 4);
   ASSERT_EQ(solution.LCA(9, 10), 6);
@@ -44,9 +50,11 @@ TEST(LCA, DifferentGraphStructure) {
 }
 
 TEST(LCA, YetAnotherGraph) {
-  std::vector<std::vector<int>> graph{
-      {1, 2}, {0, 3, 4}, {0, 5, 6}, {1, 7, 8}, {1, 9, 10}, {2}, {2, 11, 12},
-      {3},    {3},       {4},       {4},       {6},        {6}};
+  std::map<int, std::vector<int>> graph{
+      {0, {1, 2}},     {1, {0, 3, 4}}, {2, {0, 5, 6}},   {3, {1, 7, 8}},
+      {4, {1, 9, 10}}, {5, {2}},       {6, {2, 11, 12}}, {7, {3}},
+      {8, {3}},        {9, {4}},       {10, {4}},        {11, {6}},
+      {12, {6}}};
   Solution solution(graph, 0);
   ASSERT_EQ(solution.LCA(7, 8), 3);
   ASSERT_EQ(solution.LCA(9, 10), 4);
@@ -55,12 +63,13 @@ TEST(LCA, YetAnotherGraph) {
 }
 
 TEST(LCA, ComplexGraphStructure) {
-  std::vector<std::vector<int>> graph{
-      {1, 2, 3},   {0, 4, 5},   {0, 6, 7},   {0, 8, 9},   {1, 10, 11},
-      {1, 12, 13}, {2, 14, 15}, {2, 16, 17}, {3, 18, 19}, {3, 20, 21},
-      {4},         {4},         {5},         {5},         {6},
-      {6},         {7},         {7},         {8},         {8},
-      {9},         {9}};
+  std::map<int, std::vector<int>> graph{
+      {0, {1, 2, 3}},   {1, {0, 4, 5}},   {2, {0, 6, 7}},   {3, {0, 8, 9}},
+      {4, {1, 10, 11}}, {5, {1, 12, 13}}, {6, {2, 14, 15}}, {7, {2, 16, 17}},
+      {8, {3, 18, 19}}, {9, {3, 20, 21}}, {10, {4}},        {11, {4}},
+      {12, {5}},        {13, {5}},        {14, {6}},        {15, {6}},
+      {16, {7}},        {17, {7}},        {18, {8}},        {19, {8}},
+      {20, {9}},        {21, {9}}};
   Solution solution(graph, 0);
   ASSERT_EQ(solution.LCA(10, 11), 4);
   ASSERT_EQ(solution.LCA(12, 13), 5);
@@ -72,10 +81,11 @@ TEST(LCA, ComplexGraphStructure) {
 }
 
 TEST(LCA, ExtraGraph1) {
-  std::vector<std::vector<int>> graph{
-      {1, 2},      {0, 3, 4},   {0, 5, 6}, {1, 7, 8}, {1, 9, 10},
-      {2, 11, 12}, {2, 13, 14}, {3},       {3},       {4},
-      {4},         {5},         {5},       {6},       {6}};
+  std::map<int, std::vector<int>> graph{
+      {0, {1, 2}},     {1, {0, 3, 4}},   {2, {0, 5, 6}},   {3, {1, 7, 8}},
+      {4, {1, 9, 10}}, {5, {2, 11, 12}}, {6, {2, 13, 14}}, {7, {3}},
+      {8, {3}},        {9, {4}},         {10, {4}},        {11, {5}},
+      {12, {5}},       {13, {6}},        {14, {6}}};
   Solution solution(graph, 0);
   ASSERT_EQ(solution.LCA(7, 8), 3);
   ASSERT_EQ(solution.LCA(9, 10), 4);
@@ -85,10 +95,12 @@ TEST(LCA, ExtraGraph1) {
 }
 
 TEST(LCA, ExtraGraph2) {
-  std::vector<std::vector<int>> graph{
-      {1, 2},      {0, 3, 4},   {0, 5, 6}, {1, 7, 8}, {1, 9, 10}, {2, 11, 12},
-      {2, 13, 14}, {3, 15, 16}, {3},       {4},       {4},        {5},
-      {5},         {6},         {6},       {7},       {7}};
+  std::map<int, std::vector<int>> graph{
+      {0, {1, 2}},     {1, {0, 3, 4}},   {2, {0, 5, 6}},   {3, {1, 7, 8}},
+      {4, {1, 9, 10}}, {5, {2, 11, 12}}, {6, {2, 13, 14}}, {7, {3, 15, 16}},
+      {8, {3}},        {9, {4}},         {10, {4}},        {11, {5}},
+      {12, {5}},       {13, {6}},        {14, {6}},        {15, {7}},
+      {16, {7}}};
   Solution solution(graph, 0);
   ASSERT_EQ(solution.LCA(15, 16), 7);
   ASSERT_EQ(solution.LCA(9, 10), 4);
@@ -98,11 +110,12 @@ TEST(LCA, ExtraGraph2) {
 }
 
 TEST(LCA, ExtraGraph3) {
-  std::vector<std::vector<int>> graph{
-      {1, 2},      {0, 3, 4},   {0, 5, 6},   {1, 7, 8},   {1, 9, 10},
-      {2, 11, 12}, {2, 13, 14}, {3, 15, 16}, {3, 17, 18}, {4},
-      {4},         {5},         {5},         {6},         {6},
-      {7},         {7},         {8},         {8}};
+  std::map<int, std::vector<int>> graph{
+      {0, {1, 2}},      {1, {0, 3, 4}},   {2, {0, 5, 6}},   {3, {1, 7, 8}},
+      {4, {1, 9, 10}},  {5, {2, 11, 12}}, {6, {2, 13, 14}}, {7, {3, 15, 16}},
+      {8, {3, 17, 18}}, {9, {4}},         {10, {4}},        {11, {5}},
+      {12, {5}},        {13, {6}},        {14, {6}},        {15, {7}},
+      {16, {7}},        {17, {8}},        {18, {8}}};
   Solution solution(graph, 0);
   ASSERT_EQ(solution.LCA(15, 16), 7);
   ASSERT_EQ(solution.LCA(17, 18), 8);
@@ -113,7 +126,7 @@ TEST(LCA, ExtraGraph3) {
 }
 
 TEST(LCA, EmptyGraph) {
-  std::vector<std::vector<int>> graph{};
+  std::map<int, std::vector<int>> graph{};
   Solution solution(graph, 0);
   ASSERT_EQ(solution.LCA(1, 2), -1);
 }
