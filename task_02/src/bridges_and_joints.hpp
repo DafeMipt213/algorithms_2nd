56a9afe951e501a0fea3fe6ccaa1d4f85207434c
drrkkm <61612701+drrkkm@users.noreply.github.com>
2024-01-13 10:18:54 +0000

    added template support for 2 task

--- task_02/src/bridges_and_joints.hpp
+++ task_02/src/bridges_and_joints.hpp
@@ -1,25 +1,83 @@
 #pragma once
 
+#include <map>
 #include <set>
 #include <vector>
 
+template <typename T = int>
 struct Edge {
-  int first_vertex, second_vertex;
+  T first_vertex, second_vertex;
 };
 
+template <typename T = int>
 struct BridgesAndJoints {
-  std::set<Edge> bridges;
-  std::set<int> joints;
+  std::set<Edge<T>> bridges;
+  std::set<T> joints;
 
   BridgesAndJoints(){};
 };
 
-bool operator==(const Edge a, const Edge b);
-bool operator==(const BridgesAndJoints a, const BridgesAndJoints b);
-bool operator<(const Edge a, const Edge b);
+template <typename T = int>
+bool operator==(const Edge<T> a, const Edge<T> b) {
+  return (a.first_vertex == b.first_vertex &&
+          a.second_vertex == b.second_vertex) ||
+         (a.first_vertex == b.second_vertex &&
+          a.second_vertex == b.first_vertex);
+}
 
-void DFS(int vertex, std::vector<bool> &visited,
-         std::vector<std::vector<int>> &graph, std::vector<int> &depth,
-         std::vector<int> &minimum_depth, BridgesAndJoints &result,
-         int previous = -1);
-BridgesAndJoints Search(std::vector<std::vector<int>> &graph);
+template <typename T = int>
+bool operator==(const BridgesAndJoints<T> a, const BridgesAndJoints<T> b) {
+  return (a.bridges == b.bridges && a.joints == b.joints);
+}
+
+template <typename T = int>
+bool operator<(const Edge<T> a, const Edge<T> b) {
+  return (a.first_vertex < b.first_vertex && a.second_vertex < b.second_vertex);
+};
+
+template <typename T = int>
+void DFS(int vertex, std::map<T, bool> &visited,
+         std::map<T, std::vector<T>> &graph, std::map<T, int> &depth,
+         std::map<T, int> &minimum_depth, BridgesAndJoints<T> &result,
+         T previous, bool is_start = false) {
+  visited[vertex] = true;
+  minimum_depth[vertex] = depth[vertex] = (is_start ? 0 : depth[previous] + 1);
+  int children = 0;
+  for (int u : graph[vertex]) {
+    if (u != previous || is_start) {
+      if (visited[u])
+        minimum_depth[vertex] = std::min(minimum_depth[vertex], depth[u]);
+      else {
+        DFS(u, visited, graph, depth, minimum_depth, result, vertex);
+        minimum_depth[vertex] =
+            std::min(minimum_depth[vertex], minimum_depth[u]);
+        if (depth[vertex] < minimum_depth[u])
+          result.bridges.insert({vertex, u});
+        if (depth[vertex] <= minimum_depth[u] && !is_start)
+          result.joints.insert(vertex);
+
+        children++;
+      }
+    }
+  }
+  if (is_start && children > 1) result.joints.insert(vertex);
+}
+
+template <typename T = int>
+BridgesAndJoints<T> Search(std::map<T, std::vector<T>> &graph) {
+  BridgesAndJoints<T> result;
+
+  std::map<T, int> depth;
+  std::map<T, int> minimum_depth;
+  std::map<T, bool> visited;
+
+  for (auto &[vertex, _] : graph) {
+    depth[vertex] = 0;
+    minimum_depth[vertex] = 0;
+    visited[vertex] = false;
+  }
+
+  for (auto &[vertex, _] : graph)
+    DFS(vertex, visited, graph, depth, minimum_depth, result, vertex, true);
+  return result;
+}
--- task_02/src/test.cpp
+++ task_02/src/test.cpp
@@ -3,36 +3,39 @@
 #include "bridges_and_joints.hpp"
 
 TEST(BridgesAndJoints, EmptyResult) {
-  std::vector<std::vector<int>> graph = {
-      {1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3}, {0, 1, 2, 3}};
-  BridgesAndJoints result;
+  std::map<int, std::vector<int>> graph = {
+      {0, {1, 2, 3}}, {1, {0, 1, 2, 3}}, {2, {0, 1, 2, 3}}, {3, {0, 1, 2, 3}}};
+  BridgesAndJoints<int> result;
   ASSERT_EQ(Search(graph), result);
 }
 
 TEST(BridgesAndJoints, Simple) {
-  std::vector<std::vector<int>> graph = {{2}, {2}, {0, 1}};
-  BridgesAndJoints result;
+  std::map<int, std::vector<int>> graph = {{0, {2}}, {1, {2}}, {2, {0, 1}}};
+  BridgesAndJoints<int> result;
   result.bridges = {{2, 1}, {0, 2}};
   result.joints = {2};
   ASSERT_EQ(Search(graph), result);
 }
 
 TEST(BridgesAndJoints, GraphWithCycle) {
-  std::vector<std::vector<int>> graph = {{3}, {0}, {1}, {2}, {3}};
-  BridgesAndJoints result;
+  std::map<int, std::vector<int>> graph = {
+      {0, {3}}, {1, {0}}, {2, {1}}, {3, {2}}, {4, {3}}};
+  BridgesAndJoints<int> result;
   ASSERT_EQ(Search(graph), result);
 }
 
 TEST(BridgesAndJoints, Cycle) {
-  std::vector<std::vector<int>> graph = {{1}, {2}, {3}, {4}, {0, 5}, {}};
-  BridgesAndJoints result;
+  std::map<int, std::vector<int>> graph = {{0, {1}}, {1, {2}},    {2, {3}},
+                                           {3, {4}}, {4, {0, 5}}, {5, {}}};
+  BridgesAndJoints<int> result;
   result.bridges = {{4, 5}};
   result.joints = {4};
   ASSERT_EQ(Search(graph), result);
 }
 
 TEST(BridgesAndJoints, SeaStar) {
-  std::vector<std::vector<int>> graph = {{1, 2, 3, 4, 5}, {}, {}, {}, {}, {}};
+  std::map<int, std::vector<int>> graph = {
+      {0, {1, 2, 3, 4, 5}}, {1, {}}, {2, {}}, {3, {}}, {4, {}}, {5, {}}};
   BridgesAndJoints result;
   result.bridges = {{0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}};
   result.joints = {0};
@@ -40,7 +43,8 @@ TEST(BridgesAndJoints, SeaStar) {
 }
 
 TEST(BridgesAndJoints, TwoCommponents) {
-  std::vector<std::vector<int>> graph = {{1}, {2, 0}, {1}, {4}, {3, 5}, {4}};
+  std::map<int, std::vector<int>> graph = {{0, {1}}, {1, {2, 0}}, {2, {1}},
+                                           {3, {4}}, {4, {3, 5}}, {5, {4}}};
   BridgesAndJoints result;
   result.bridges = {{1, 2}, {0, 1}, {4, 5}, {3, 4}};
   result.joints = {1, 4};
@@ -48,14 +52,14 @@ TEST(BridgesAndJoints, TwoCommponents) {
 }
 
 TEST(BridgesAndJoints, EmptyGraph) {
-  std::vector<std::vector<int>> graph = {{}};
+  std::map<int, std::vector<int>> graph = {};
   BridgesAndJoints result;
   ASSERT_EQ(Search(graph), result);
 }
 
 TEST(BridgesAndJoints, SpiderGraph) {
-  std::vector<std::vector<int>> graph = {{1, 2, 3}, {0}, {0},
-                                         {0, 4, 5}, {3}, {3}};
+  std::map<int, std::vector<int>> graph = {{0, {1, 2, 3}}, {1, {0}}, {2, {0}},
+                                           {3, {0, 4, 5}}, {4, {3}}, {5, {3}}};
   BridgesAndJoints result;
   result.bridges = {{0, 1}, {0, 2}, {3, 4}, {3, 5}, {0, 3}};
   result.joints = {3, 0};
@@ -64,63 +68,66 @@ TEST(BridgesAndJoints, SpiderGraph) {
 }
 
 TEST(BridgesAndJoints, OnlySimpleCycle) {
-  std::vector<std::vector<int>> graph = {{0}, {1}, {2}};
+  std::map<int, std::vector<int>> graph = {{0, {0}}, {1, {1}}, {2, {2}}};
   BridgesAndJoints result;
   result.bridges = {{1, 2}, {0, 1}, {4, 5}, {3, 4}};
   result.joints = {4, 1};
   result = Search(graph);
   ASSERT_EQ(Search(graph), result);
 }
+
 TEST(BridgesAndJoints, NoBridgesNoJoints) {
-  std::vector<std::vector<int>> graph = {{0}, {1}, {2}};
+  std::map<int, std::vector<int>> graph = {{0, {0}}, {1, {1}}, {2, {2}}};
   BridgesAndJoints result;
   ASSERT_EQ(Search(graph), result);
 }
 
 TEST(BridgesAndJoints, OneBridgeNoJoints) {
-  std::vector<std::vector<int>> graph = {{1}, {0}};
+  std::map<int, std::vector<int>> graph = {{0, {1}}, {1, {0}}};
   BridgesAndJoints result;
   result.bridges = {{0, 1}};
   ASSERT_EQ(Search(graph), result);
 }
 
 TEST(BridgesAndJoints, MultipleBridgesAndJoints) {
-  std::vector<std::vector<int>> graph = {{},
-                                         {2, 3, 4, 5, 9, 10},
-                                         {1, 3, 6, 8},
-                                         {1, 2, 8, 9},
-                                         {1, 6, 7, 8, 9, 10},
-                                         {1},
-                                         {2, 4, 7, 9},
-                                         {4, 6, 8},
-                                         {2, 3, 4, 7, 9},
-                                         {1, 3, 4, 6, 8},
-                                         {1, 4}};
+  std::map<int, std::vector<int>> graph = {{0, {}},
+                                           {1, {2, 3, 4, 5, 9, 10}},
+                                           {2, {1, 3, 6, 8}},
+                                           {3, {1, 2, 8, 9}},
+                                           {4, {1, 6, 7, 8, 9, 10}},
+                                           {5, {1}},
+                                           {6, {2, 4, 7, 9}},
+                                           {7, {4, 6, 8}},
+                                           {8, {2, 3, 4, 7, 9}},
+                                           {9, {1, 3, 4, 6, 8}},
+                                           {10, {1, 4}}};
   BridgesAndJoints result;
   result.bridges = {{5, 1}};
   result.joints = {1};
   ASSERT_EQ(Search(graph), result);
 }
 
-TEST(bridges_and_joints, FishGraph) {
-  std::vector<std::vector<int>> graph = {{1, 2, 3},       {0, 2, 3}, {0, 1, 3},
-                                         {0, 1, 2, 4, 5}, {3, 5},    {3, 4}};
+TEST(BridgesAndJoints, FishGraph) {
+  std::map<int, std::vector<int>> graph = {{0, {1, 2, 3}}, {1, {0, 2, 3}},
+                                           {2, {0, 1, 3}}, {3, {0, 1, 2, 4, 5}},
+                                           {4, {3, 5}},    {5, {3, 4}}};
   BridgesAndJoints result;
   result.joints = {3};
   ASSERT_EQ(Search(graph), result);
 }
 
-TEST(bridges_and_joints, ComplexGraphFirst) {
-  std::vector<std::vector<int>> graph = {{1, 2},    {0, 2, 3, 4}, {0, 1, 4},
-                                         {1, 4, 5}, {1, 2, 3, 5}, {3, 4}};
+TEST(BridgesAndJoints, ComplexGraphFirst) {
+  std::map<int, std::vector<int>> graph = {{0, {1, 2}},       {1, {0, 2, 3, 4}},
+                                           {2, {0, 1, 4}},    {3, {1, 4, 5}},
+                                           {4, {1, 2, 3, 5}}, {5, {3, 4}}};
   BridgesAndJoints result;
   ASSERT_EQ(Search(graph), result);
 }
 
-TEST(bridges_and_joints, ComplexGraphSecond) {
-  std::vector<std::vector<int>> graph = {{1, 2, 3, 4},    {0, 2, 3, 4},
-                                         {0, 1, 3, 4},    {0, 1, 2, 4, 5},
-                                         {0, 1, 2, 3, 5}, {3, 4}};
+TEST(BridgesAndJoints, ComplexGraphSecond) {
+  std::map<int, std::vector<int>> graph = {
+      {0, {1, 2, 3, 4}},    {1, {0, 2, 3, 4}},    {2, {0, 1, 3, 4}},
+      {3, {0, 1, 2, 4, 5}}, {4, {0, 1, 2, 3, 5}}, {5, {3, 4}}};
   BridgesAndJoints result;
   ASSERT_EQ(Search(graph), result);
 }
