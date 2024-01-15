58c9c0d91e3770972c7b3819f772e7115516087b
CHICH <118026077+AldMatvey@users.noreply.github.com>
2024-01-15 00:53:55 +0000

    added extra task - PRIM

--- additional_tasks/template_task/src/main.cpp
+++ additional_tasks/template_task/src/main.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
+#include <vector>
 
-int main() { return 0; }
+#include "prim.hpp"
+
+int main() {
+  std::vector<std::vector<Vertex>> graph{
+      {{1, 1}, {2, 7}, {3, 3}, {4, 7}, {5, 10}, {6, 1}},
+      {{0, 1}, {2, 3}, {3, 1}},
+      {{0, 7}, {1, 3}, {3, 4}},
+      {{0, 3}, {1, 1}, {2, 4}, {4, 2}},
+      {{3, 2}, {0, 7}, {5, 3}},
+      {{4, 3}, {0, 10}, {6, 4}},
+      {{0, 1}, {5, 4}}};
+  std::vector<std::vector<Vertex>> tree = prim(graph);
+  return 0;
+}
--- additional_tasks/template_task/src/test.cpp
+++ additional_tasks/template_task/src/test.cpp
@@ -3,6 +3,36 @@
 
 #include <stack>
 
-#include "utils.hpp"
+#include "prim.hpp"
 
-TEST(Template, Simple) { ASSERT_EQ(true, true); }
+TEST(Prim, BigGraph) {
+  std::vector<std::vector<Vertex>> graph{
+      {{1, 1}, {2, 7}, {3, 3}, {4, 7}, {5, 10}, {6, 1}},
+      {{0, 1}, {2, 3}, {3, 1}},
+      {{0, 7}, {1, 3}, {3, 4}},
+      {{0, 3}, {1, 1}, {2, 4}, {4, 2}},
+      {{3, 2}, {0, 7}, {5, 3}},
+      {{4, 3}, {0, 10}, {6, 4}},
+      {{0, 1}, {5, 4}}};
+  std::vector<std::vector<Vertex>> res{
+      {{1, 1}, {6, 1}}, {{0, 1}, {3, 1}, {2, 3}}, {{1, 3}},
+      {{1, 1}, {4, 2}}, {{3, 2}, {5, 3}},         {{4, 3}},
+      {{0, 1}}};
+  std::vector<std::vector<Vertex>> primres = prim(graph);
+  ASSERT_EQ(prim(graph), res);
+}
+
+TEST(Prim, SimpleGraph) {
+  std::vector<std::vector<Vertex>> graph{
+      {{1, 3}, {2, 1000}}, {{0, 3}, {2, 3}}, {{1, 3}, {0, 1000}}};
+  std::vector<std::vector<Vertex>> res{{{1, 3}}, {{0, 3}, {2, 3}}, {{1, 3}}};
+  std::vector<std::vector<Vertex>> primres = prim(graph);
+  ASSERT_EQ(prim(graph), res);
+}
+
+TEST(Prim, EmptyGraph) {
+  std::vector<std::vector<Vertex>> graph{};
+  std::vector<std::vector<Vertex>> res{};
+  std::vector<std::vector<Vertex>> primres = prim(graph);
+  ASSERT_EQ(prim(graph), res);
+}
