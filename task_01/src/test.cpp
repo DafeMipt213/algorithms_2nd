f35b35d20d13edd8698341bad4ba48b89507c9a1
G00sya <118297271+G00sya@users.noreply.github.com>
2024-01-15 07:20:13 +0000

    Added one more test for TS

--- task_01/src/test.cpp
+++ task_01/src/test.cpp
@@ -8,3 +8,10 @@ TEST(TopologySort, Simple) {
   std::vector<int> correct_ans{0, 3, 2, 1};
   ASSERT_EQ(answer, correct_ans);
 }
+
+TEST(TopologySort, Normal) {
+  Graph g(6, {{0, {1, 2}}, {1, {}}, {2, {3}}, {3, {4}}, {4, {5}}, {5, {}}});
+  std::vector<int> answer = g.Sort();
+  std::vector<int> correct_ans{0, 2, 3, 4, 5, 1};
+  ASSERT_EQ(answer, correct_ans);
+}
