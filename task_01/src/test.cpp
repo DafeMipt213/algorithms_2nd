1d73ba56ee1ee73a60871dd26ce657aeb37a18d7
G00sya <118297271+G00sya@users.noreply.github.com>
2024-01-15 07:14:45 +0000

    Added simple test for topology sort

--- task_01/src/test.cpp
+++ task_01/src/test.cpp
@@ -1,6 +1,10 @@
 #include <gtest/gtest.h>
+#include <vector>
 #include "topology_sort.hpp"
 
 TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+  Graph g(4, {{0, {3}}, {1, {}}, {2, {1}}, {3, {1, 2}}});
+  std::vector<int> answer = g.Sort();
+  std::vector<int> correct_ans{0, 3, 2, 1};
+  ASSERT_EQ(answer, correct_ans);
 }
